f7c44dff0db23da519be7e2b271bd6a4847eedfa
Caio Granero <caio.granero@usp.br>
2017-08-17 21:00:03 -0300

    Alterei a consulta para trazer apenas os dados necessários e filtrar somente pelos dados da SPTrans

--- api/models/Routes.js
+++ api/models/Routes.js
@@ -9,24 +9,22 @@ const Stops = {
                 SHAPES.shape_id,    
                 SHAPES.shape_pt_lon,
                 SHAPES.shape_pt_lat,
-                SHAPES.shape_pt_sequence,
-                TRIPS.trip_id,
-                TRIPS.trip_headsign,
-                STOPS.stop_id,
                 STOPS.stop_name,
                 STOPS.stop_lat,
                 STOPS.stop_lon
             FROM gtfs_engine_shapes AS SHAPES
             INNER JOIN gtfs_engine_trips AS TRIPS ON SHAPES.shape_id = TRIPS.shape_id
+            INNER JOIN gtfs_engine_routes AS ROUTES ON TRIPS.route_id = ROUTES.route_id
             INNER JOIN gtfs_engine_stop_times AS STOP_TIMES ON STOP_TIMES.trip_id = TRIPS.trip_id
             INNER JOIN gtfs_engine_stops AS STOPS ON STOPS.stop_id = STOP_TIMES.stop_id
 
             WHERE 
+                ROUTES.route_type = 3 AND 
                 ST_DWithin(
                     STOPS.stop_point, 
                     ST_SetSRID(
                         ST_MakePoint(<!-- @echo lng -->, <!-- @echo lat -->), 4326
-                    ), 0.3/111.325
+                    ), 0.15/111.325
                 )
             
             ORDER BY shape_id, shape_pt_sequence`
